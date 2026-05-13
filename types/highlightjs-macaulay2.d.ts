4e06e29fd7e42c51b18df0165aaa7e639f5bf0ba
Doug Torrance <dtorrance@piedmont.edu>
2026-05-13 06:59:04 -0400

    Don't use augmentation syntax when not augmenting

--- types/highlightjs-macaulay2.d.ts
+++ types/highlightjs-macaulay2.d.ts
@@ -1,6 +1,4 @@
 import { LanguageFn } from "highlight.js";
 
-declare module "highlightjs-macaulay2" {
-  const macaulay2: LanguageFn;
-  export default macaulay2;
-}
+declare const macaulay2: LanguageFn;
+export default macaulay2;
